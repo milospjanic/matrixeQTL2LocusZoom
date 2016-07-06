8e169d0c0f6f527c7e45ab947c747e5b20c5d787
Milos Pjanic <pjanic.milos@gmail.com>
2016-07-06 12:29:14 -0700

    Update matrixeQTL2LocusZoom.sh

--- matrixeQTL2LocusZoom.sh
+++ matrixeQTL2LocusZoom.sh
@@ -8,7 +8,7 @@ tr -d "\"" < $1.eQTL.input > $1.eQTL.input.noquote
 tail -n+2 $1.eQTL.input.noquote > $1.eQTL.input.noquote.noheader
 cut -f1,4 $1.eQTL.input.noquote.noheader > $1.eQTL.input.noquote.noheader.cut
 sed "s/$2\://g" $1.eQTL.input.noquote.noheader.cut > $1.eQTL.input.noquote.noheader.cut.sed
-
+cp $1.eQTL.input.noquote.noheader.cut.sed tmp1
 #Remove temp files
 rm $1.eQTL.input
 rm $1.eQTL.input.noquote
@@ -20,7 +20,9 @@ grep "chr$2" snp142.cut_tab > snp142.cut_tab.chr$2
 grep -v "chr$2_" snp142.cut_tab.chr$2 >snp142.cut_tab.chr$2_clean
 
 #Create output
-awk -F " " 'BEGIN{while(getline<""'"$1"'".eQTL.input.noquote.noheader.cut.sed") a[$1]=1&&b[$1]=$2 } ; a[$3] ==1 {print $4, b[$3] } ' snp142.cut_tab.chr"$2"_clean >$1.eQTL.input.noquote.noheader.cut.sed.rsid
+awk -F " " 'BEGIN{while(getline<"tmp1") a[$1]=1&&b[$1]=$2 } ; a[$3] ==1 {print $4, b[$3] } ' snp142.cut_tab.chr"$2"_clean > tmp2
+mv tmp2 $1.eQTL.rsIDs
 
 #Remove temp file
+rm tmp1
 rm $1.eQTL.input.noquote.noheader.cut.sed
