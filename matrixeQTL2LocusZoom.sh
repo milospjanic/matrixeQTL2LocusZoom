ff047631daf2f9a5a8117fa384f00139bca0af02
Milos Pjanic <pjanic.milos@gmail.com>
2016-07-06 11:21:50 -0700

    Update matrixeQTL2LocusZoom.sh

--- matrixeQTL2LocusZoom.sh
+++ matrixeQTL2LocusZoom.sh
@@ -7,7 +7,7 @@ grep $1 $3 > $1.eQTL.input
 tr -d "\"" < $1.eQTL.input > $1.eQTL.input.noquote
 tail -n+2 $1.eQTL.input.noquote > $1.eQTL.input.noquote.noheader
 cut -f1,4 $1.eQTL.input.noquote.noheader > $1.eQTL.input.noquote.noheader.cut
-sed 's/6\://g' $1.eQTL.input.noquote.noheader.cut > $1.eQTL.input.noquote.noheader.cut.sed
+sed "s/$2\://g" $1.eQTL.input.noquote.noheader.cut > $1.eQTL.input.noquote.noheader.cut.sed
 
 #Remove temp files
 rm $1.eQTL.input
@@ -20,7 +20,7 @@ grep "chr$2" snp142.cut_tab > snp142.cut_tab.chr$2
 grep -v "chr$2_" snp142.cut_tab.chr$2 >snp142.cut_tab.chr$2_clean
 
 #Create output
-awk -F " " 'BEGIN{while(getline<"$1.eQTL.input.noquote.noheader.cut.sed") a[$1]=1&&b[$1]=$2 } ; a[$3] ==1 {print $4, b[$3] } ' snp142.cut_tab.chr$2_clean >$1.eQTL.input.noquote.noheader.cut.sed.rsid
+awk -F " " 'BEGIN{while(getline<""'"$1"'".eQTL.input.noquote.noheader.cut.sed") a[$1]=1&&b[$1]=$2 } ; a[$3] ==1 {print $4, b[$3] } ' snp142.cut_tab.chr"$2"_clean >$1.eQTL.input.noquote.noheader.cut.sed.rsid
 
 #Remove temp file
 rm $1.eQTL.input.noquote.noheader.cut.sed
